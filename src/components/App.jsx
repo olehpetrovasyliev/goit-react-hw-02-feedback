377b172c14e643ebcd48eac2f0368a0b33b24ee6
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2023-06-21 17:16:58 +0300

    adds buttons

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,5 @@
+import { FeedbackBtns } from './FeedbackBtns/FeedbackBtns';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <FeedbackBtns />;
 };
---
+++ src/components/FeedbackBtns/FeedbackBtns.styled.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const StyledButton = styled.button`
+  padding: 12px 24px;
+  border-radius: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const FlexWrapper = styled.div`
+  display: flex;
+  gap: 16px;
+`;
